7219bb7ed5b3f697c25215f1ee470992ee609671
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-10-29 14:48:00 -0500

    likes and comments

--- galleryoflaura/server/public/scripts/client.js
+++ galleryoflaura/server/public/scripts/client.js
@@ -8,16 +8,25 @@ myApp.controller('LauraController', function(){  //creating controller
     var laura = this;
     laura.photos = pictures.data;
     laura.hideShow = hideShow;
+    laura.like = like;
+    laura.addComment = addComment;
 
     //setting up objects for .data
- 
-   function hideShow(lphotos) {
+  
+    function hideShow(lphotos) {
       console.log(lphotos);
       lphotos.showText = !lphotos.showText;
       if(lphotos.showText) {
         lphotos.views++;
       }
     } //click function for picture to show descrpition
-      
-    
+  
+    function addComment(lphotos, newcomment) {
+        lphotos.comments.push(newcomment);
+        lphotos.newComment = '';
+    }  //add comment to comments array
+  
+    function like(lphotos) {
+        lphotos.likes++;
+    } //increment photo like
   });
