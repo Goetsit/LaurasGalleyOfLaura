47fd355d9f3e696ebaaa8b247c96ec4915409e3c
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-10-29 14:00:30 -0500

    data

---
+++ galleryoflaura/server/public/scripts/data.js
@@ -0,0 +1,26 @@
+var pictures = {};
+pictures.data = [
+  {imageid: 1, filepath: 'images/BenLutzButz.jpg', description:'Ben wearing Krista\'s sunglasses at the corgi races. I think he pulls them off.'},
+  {imageid: 2, filepath: 'images/LauraDadAfton.jpg', description:'Dad and I at Afton 2017.'},
+  {imageid: 3, filepath: 'images/Frowns.jpg', description:'There might be only two photos of Ben and I together, and this is one of them.'},
+  {imageid: 4, filepath: 'images/EricHarms.jpg', description:'Suicide Walk in memory of Eric Harms 2016.'},
+  {imageid: 5, filepath: 'images/ArmBreak.jpg', description:'I broke my arm snowboarding.'},
+  {imageid: 6, filepath: 'images/BenButtz.jpg', description:'The glorious Benjamin Lutz.'},
+  {imageid: 7, filepath: 'images/BeachShark.jpg', description:'2016 trip to Myrtle Beach.'},
+  {imageid: 8, filepath: 'images/BeachPinkThings.jpg', description:'Also during 2016 trip to Myrtle Beach.'},
+  {imageid: 9, filepath: 'images/DadLauraDads50.jpg', description:'My dad and I at his 50th birthday party.'},
+  {imageid: 10, filepath: 'images/Halloween2016Whiskey.jpg', description:'I really like Whiskey, can you guess who I was dressed as?.'},
+  {imageid: 11, filepath: 'images/LauraKristaPride2017.jpg', description:'Krista and I volunteered for Pride in 2017.'},
+  {imageid: 12, filepath: 'images/Lutsen.jpg', description:'Keeping things classy, pre arm break Lutsen 2017.'},
+  {imageid: 13, filepath: 'images/SupportNerds.jpg', description:'Greatest People I have ever worked with. Some of these people are my best friends.'},
+  {imageid: 14, filepath: 'images/RobbLauraDad2017.jpg', description:'Robb, me, Dad at the wedding of Pam and Adam 2017.'},
+  {imageid: 15, filepath: 'images/sapphire.jpg', description:'Childhood dog, Sapphire.'},
+  {imageid: 16, filepath: 'images/valfoxxx.jpg', description:'Val Fox and I, Avionte CLient Connect 2017.'},
+  {imageid: 17, filepath: 'images/roommatechristmas.jpg', description:'roommate christmas 2015, I was proud of out little tree.'},
+  {imageid: 18, filepath: 'images/christmaswife.jpg', description:'I got my wife a globe bar, she was really excited to open it.'},
+  {imageid: 19, filepath: 'images/poppop.jpg', description:'Laura as a small child and her grandpa "pop-pop".'},
+  {imageid: 20, filepath: 'images/PeopleILike.jpg', description:'Christmas time beers with Frank, Joe, Paige, and Ben.'},
+  {imageid: 21, filepath: 'images/Wife.jpg', description:'This is my wife. My absolute best friend, Krista'},
+  {imageid: 22, filepath: 'images/Brother.jpg', description:'Brother Jeremy and my Christmas gift last year.'},
+  
+];
