dc8e8f8fb2093ce1187b458238f4d3051846e2d4
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-10-29 14:39:34 -0500

    added description

--- galleryoflaura/server/public/scripts/client.js
+++ galleryoflaura/server/public/scripts/client.js
@@ -7,7 +7,17 @@ myApp.controller('LauraController', function(){  //creating controller
     console.log('laura controller is a thing');
     var laura = this;
     laura.photos = pictures.data;
+    laura.hideShow = hideShow;
 
     //setting up objects for .data
  
+   function hideShow(lphotos) {
+      console.log(lphotos);
+      lphotos.showText = !lphotos.showText;
+      if(lphotos.showText) {
+        lphotos.views++;
+      }
+    } //click function for picture to show descrpition
+      
+    
   });
