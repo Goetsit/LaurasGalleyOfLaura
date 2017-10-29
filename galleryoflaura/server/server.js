97ab5f2db67c428e744841433e2abbbb86fa90c5
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-10-29 15:02:53 -0500

    server wouldn't start, updated file path

--- galleryoflaura/server/server.js
+++ galleryoflaura/server/server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 // Serve up static files
-app.use(express.static('server/public'));
+app.use(express.static('public'));
+
+app.listen(port, function(){
+    console.log('listening on port', port);
+  });
